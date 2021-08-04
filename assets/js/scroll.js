ca3fdaf09dce0387335e3848c94da47339e358de
sun123zxy <304244450@qq.com>
2021-08-05 00:19:56 +0800

    background scroller for Firefox

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,5 +1,21 @@
-$(window).scroll(function(e) {
-    var cur = $(this).scrollTop();
-    var tot = document.body.scrollHeight;
-    $("body").css("background-position","0% "+String(cur/tot*100) + "%");
-});
+mxScrollSpeed = 0.007
+console.log("background scroller loaded")
+
+userAgent = navigator.userAgent
+if (userAgent.indexOf("Firefox") > -1) {
+    $(window).scroll(function(e) {
+        var cur = $(this).scrollTop();
+        var tot = document.body.scrollHeight;
+        var pos;
+        if(mxScrollSpeed * tot <= 100){ //speed control
+            pos = mxScrollSpeed * cur;
+        }else{
+            pos = cur/tot*100;
+        }
+        $("body").css("background-position","0% "+ pos + "%");
+    });
+    console.log("background scroller activated");
+}else{
+    console.log("error: background scroller supports Firefox only");
+    console.log("background scroller inactivated");
+}
