a5eb67a640e83f6be70c5ed7f5110714be63deb4
sun123zxy <304244450@qq.com>
2022-04-10 00:42:26 +0800

    music player, round button css flex

---
+++ assets/js/music_player.js
@@ -0,0 +1,11 @@
+isMusicPlayerActivated = false;
+$("#music-player .round-button").click(function(){
+    if(isMusicPlayerActivated == false){
+        $("#music-player .hover-info").addClass("music-player-activated");
+        $("#music-player .hover-info").html(
+            '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=450 src="//music.163.com/outchain/player?type=0&id=5244816970&auto=1&height=430"></iframe>'
+        );
+        isMusicPlayerActivated = true;
+        console.log("Music player activated!")
+    }
+});
