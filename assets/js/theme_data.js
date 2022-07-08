e110ed90a92a400075a5725fb2a4a7357eb572fe
sun123zxy <304244450@qq.com>
2022-07-08 15:55:41 +0800

    theme work

--- assets/js/theme_data.js
+++ assets/js/theme_data.js
@@ -7,17 +7,24 @@ var themesDat = [
             "/assets/images/bg/netherStairs.jpg"
         ]
     },{
-        "src": themeSrc + "bambooForest.css",
-        "info": "bambooForest, 月下の永遠亭, pixiv pid=26565744",
+        "src": themeSrc + "netherStairs_dusk.css",
+        "info": "netherStairs_dusk, ゆきどけアゼリア, pixiv pid=50046630, modified",
         "night": true,
         "preloads": [
-            "/assets/images/bg/bambooForest.jpg"
+            "/assets/images/bg/netherStairs_dusk.jpg"
         ]
     },{
         "src": themeSrc + "hakureiShrine.css",
-        "info": "hakureiShrine, 幻想回帰, pixiv pid=76561447",
+        "info": "hakureiShrine, 幻想回帰, pixiv pid=76561447, modified",
         "preloads": [
             "/assets/images/bg/hakureiShrine.jpg"
+        ]
+    },{
+        "src": themeSrc + "bambooForest.css",
+        "info": "bambooForest, 月下の永遠亭, pixiv pid=26565744, modified",
+        "night": true,
+        "preloads": [
+            "/assets/images/bg/bambooForest.jpg"
         ]
     }
 ]
